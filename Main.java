c97f80d663be0b519644149fe1cd9db0d258940b
Rahul Bag <rahulbag722136@gmail.com>
2026-03-09 04:36:18 +0530

    Added Song class and updated project

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Song song1 = new Song(
+                "Shape of You",
+                "Ed Sheeran",
+                "Divide",
+                240
+        );
+
+        song1.displaySongInfo();
+
+        System.out.println(song1);
+    }
+}
--- Song.java
+++ Song.java
@@ -3,8 +3,9 @@ public class Song {
     private String title;
     private String artist;
     private String album;
-    private int duration; // seconds
+    private int duration;
 
+    // Constructor
     public Song(String title, String artist, String album, int duration) {
         this.title = title;
         this.artist = artist;
@@ -12,11 +13,49 @@ public class Song {
         this.duration = duration;
     }
 
-    public String getTitle() { return title; }
-    public String getArtist() { return artist; }
-    public String getAlbum() { return album; }
-    public int getDuration() { return duration; }
+    // Getter Methods
+    public String getTitle() {
+        return title;
+    }
 
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    // Setter Methods
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    // Display Song Information
+    public void displaySongInfo() {
+        System.out.println("Song Title : " + title);
+        System.out.println("Artist     : " + artist);
+        System.out.println("Album      : " + album);
+        System.out.println("Duration   : " + duration + " seconds");
+    }
+
+    // toString Method
     @Override
     public String toString() {
         return title + " - " + artist + " (" + duration + " sec)";
